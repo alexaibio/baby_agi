3f66706e1df2169407483257ed2b4bf8c0039068
Alex Y <github.alex.ai@gmail.com>
2023-05-25 18:18:50 +0300

    Initial commit

---
+++ baby_AGI.py
@@ -0,0 +1,42 @@
+'''
+PERSONAL ASSISTANT with LangChain and OpenAI
+https://python.langchain.com/en/latest/use_cases/agents/baby_agi.html
+https://python.langchain.com/en/latest/use_cases/personal_assistants.html
+'''
+
+from typing import Dict, List, Optional, Any
+from langchain import LLMChain, OpenAI, PromptTemplate
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.base import Chain
+from langchain.vectorstores import FAISS
+from langchain.docstore import InMemoryDocstore
+import faiss
+from my_controller import BabyAGI
+import openai
+from settings import config
+openai.api_key = config["OPENAI_API_KEY"]
+
+
+# Define your embedding model
+embeddings_model = OpenAIEmbeddings()
+
+######## Faiss: A library for efficient similarity search
+# Initialize the vector store as empty
+embedding_size = 1536
+index = faiss.IndexFlatL2(embedding_size)
+vectorstore = FAISS(embeddings_model.embed_query, index, InMemoryDocstore({}), {})
+
+# initialize the LLM model
+llm = OpenAI(temperature=0)  # define a model
+
+# initialize BabyAGI
+verbose = False   # Logging of LLMChains
+max_iterations: Optional[int] = 5   # If None, will keep on and going forever
+baby_agi = BabyAGI.from_llm(llm=llm, vectorstore=vectorstore, verbose=verbose, max_iterations=max_iterations)
+
+# run it
+OBJECTIVE = "Help me to find a role of PDE10A in treatment of psychomotor agitation "
+print()
+print(f'OBJECTIVE: {OBJECTIVE}')
+baby_agi({"objective": OBJECTIVE})
+
---
+++ my_chain.py
@@ -0,0 +1,71 @@
+from langchain import LLMChain, OpenAI, PromptTemplate
+from langchain.llms import BaseLLM
+
+
+class TaskCreationChain(LLMChain):
+    """Chain to generate tasks."""
+
+    @classmethod
+    def from_llm(cls, llm: BaseLLM, verbose: bool = True) -> LLMChain:
+        """Get the response parser."""
+        task_creation_template = (
+            "You are a task creation AI that uses the result of an execution agent"
+            " to create new tasks with the following objective: {objective},"
+            " The last completed task has the result: {result}."
+            " This result was based on this task description: {task_description}."
+            " These are incomplete tasks: {incomplete_tasks}."
+            " Based on the result, create 2 new tasks to be completed, "
+            " by the AI system that do not overlap with incomplete tasks."
+            " Return the tasks as an array."
+        )
+        prompt = PromptTemplate(
+            template=task_creation_template,
+            input_variables=[
+                "result",
+                "task_description",
+                "incomplete_tasks",
+                "objective",
+            ],
+        )
+        return cls(prompt=prompt, llm=llm, verbose=verbose)
+
+
+class TaskPrioritizationChain(LLMChain):
+    """Chain to prioritize tasks."""
+
+    @classmethod
+    def from_llm(cls, llm: BaseLLM, verbose: bool = True) -> LLMChain:
+        """Get the response parser."""
+        task_prioritization_template = (
+            "You are a task prioritization AI tasked with cleaning the formatting of and reprioritizing"
+            " the following tasks: {task_names}."
+            " Consider the ultimate objective of your team: {objective}."
+            " Do not remove any tasks. Do not create any new tasks. Return the result as a numbered list, like:"
+            " #. First task"
+            " #. Second task"
+            " Start the task list with number {next_task_id}."
+        )
+        prompt = PromptTemplate(
+            template=task_prioritization_template,
+            input_variables=["task_names", "next_task_id", "objective"],
+        )
+        return cls(prompt=prompt, llm=llm, verbose=verbose)
+
+
+class ExecutionChain(LLMChain):
+    """Chain to execute tasks."""
+
+    @classmethod
+    def from_llm(cls, llm: BaseLLM, verbose: bool = True) -> LLMChain:
+        """Get the response parser."""
+        execution_template = (
+            "You are an AI who performs one task based on the following objective: {objective}."
+            " Take into account these previously completed tasks: {context}."
+            " Your task: {task}."
+            " Response:"
+        )
+        prompt = PromptTemplate(
+            template=execution_template,
+            input_variables=["objective", "context", "task"],
+        )
+        return cls(prompt=prompt, llm=llm, verbose=verbose)
